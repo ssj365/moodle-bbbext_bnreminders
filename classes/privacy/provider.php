c24f417afb4f65318732e7fe653989b518d5cb5a
Shamiso Jaravaza <33659194+ssj365@users.noreply.github.com>
2025-10-10 09:45:08 -0600

    GIT-4 bbbext_bnreminders: Fix privacy provider
    Fix #4
     * Added support for user preferences

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -16,6 +16,10 @@
 
 namespace bbbext_bnreminders\privacy;
 
+use core_privacy\local\request\transform;
+use core_privacy\local\request\writer;
+use core_privacy\local\metadata\collection;
+
 /**
  * Privacy class for requesting user data.
  *
@@ -24,15 +28,40 @@ namespace bbbext_bnreminders\privacy;
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author    Jesus Federico  (jesus [at] blindsidenetworks [dt] com)
  */
-class provider implements \core_privacy\local\metadata\null_provider {
+class provider implements
+    // This plugin has data to export.
+    \core_privacy\local\metadata\provider,
+    // This plugin has user preferences to export.
+    \core_privacy\local\request\user_preference_provider {
+
     /**
-     * Get the language string identifier with the component's language
-     * file to explain why this plugin stores no data.
+     * Provides metadata about the personal data stored by the plugin.
      *
-     * @return  string
+     * @param collection $collection The metadata collection to be updated.
+     * @return collection The updated metadata collection.
      */
-    public static function get_reason(): string {
-        return 'privacy:metadata';
+    public static function get_metadata(collection $collection): collection {
+        $collection->add_user_preference('bbbext_bnreminders',
+            'privacy:metadata:preference:bbbext_bnreminders');
+        return $collection;
+    }
+
+    /**
+     * Export the user preference in a readable way per activity.
+     * @param int $userid The user to export data for.
+     */
+    public static function export_user_preferences(int $userid): void {
+        $userpref = get_user_preferences('bbbext_bnreminders', '', $userid);
+        $desc = '';
+        if ($userpref !== null) {
+            $preferences = json_decode($userpref, true);
+            foreach ($preferences as $bbbinstanceid => $enabled) {
+                $preference = $enabled ? 'privacy:reminderpreferenceyes' : 'privacy:reminderpreferenceno';
+                $userprefdescription = get_string($preference, 'bbbext_bnreminders', ['activityid' => $bbbinstanceid]);
+                $desc .= $userprefdescription . " ";
+            }
+            writer::export_user_preference('bbbext_bnreminders', 'bbbext_bnreminders', $userpref,
+                rtrim($desc));
+        }
     }
 }
-
